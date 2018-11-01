c0c84c905af33b136052cd1673a8dcc03d58722d
Luann Santos <luann.santos@com4ad.local>
2018-11-01 18:23:05 -0300

    .

--- rest-api/model/portifolio/PortifolioModel.js
+++ rest-api/model/portifolio/PortifolioModel.js
@@ -1,7 +1,7 @@
 const db = require('../../banco/dbconnection');
 
 module.exports = class PortifolioModel {
-    // metodo para recuparar todos os portiifolio do banco
+    // metodo para recuperar todos os portiifolio do banco
     static getTodos(callback){
         return db.query("select * from portifolio", callback);
     }
@@ -11,7 +11,7 @@ module.exports = class PortifolioModel {
     }
     // metodo para adicionar um portifolio
     static adicionar(portifolio, callback){
-        return db.query("insert into portifolio(descricao, detalhes) values (?, ?)", [portifolio.descricao, portifolio.detalhes], callback);
+        return db.query("insert into portifolio(nome, descricao, detalhes) values (?, ?, ?)", [portifolio.nome, portifolio.descricao, portifolio.detalhes], callback);
     }
     // metodo para excluir um portifolio
     static deletar(id, callback){
@@ -19,8 +19,7 @@ module.exports = class PortifolioModel {
     }
     // metodo para editar um portifolio
     static editar(portifolio, callback){
-        return db.query("update portifolio set descricao = ?, detalhes = ? where id_portifolio = ?", [portifolio.descricao, portifolio.detalhes, portifolio.id_portifolio], callback);
+        return db.query("update portifolio set nome = ?, descricao = ?, detalhes = ? where id_portifolio = ?", [portifolio.nome, portifolio.descricao, portifolio.detalhes, portifolio.id_portifolio], callback);
     }
-
 
 };
